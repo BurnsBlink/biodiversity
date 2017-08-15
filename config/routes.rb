215cf12b062414e23be48b921d41319b9bcea3bb
Sam <samuel.burns.25@gmail.com>
2017-08-15 14:10:30 -0400

    core page set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  resources :pages, only: [:index]
+  root 'pages#index'
+
+  resources :cladograms, only: [:index] do
+    resources :bacteria, only: [:index]
+    resources :archaea, only: [:index]
+    resources :excavata, only: [:index]
+    resources :unikonta, only: [:index]
+    resources :archaeplastida, only: [:index]
+    resources :sar, only: [:index]
+  end
+
 end
